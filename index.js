2fbb971e94e1e24ea8b19884f43b3e63b3f26e1a
gagangani13 <127178242+gagangani13@users.noreply.github.com>
2023-03-19 11:48:43 +0530

    Async Await..using async await

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 console.log("person1:Entered theatre")
 console.log("person2:Entered theatre")
-const Dadisontheway=new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        resolve('person3:Got ticket')
-    },2000)
-})
-const getPopcorn=Dadisontheway.then((t)=>{
-    return new Promise((resolve,reject)=>{
-        resolve(`${t}, bought Popcorn`)
+const person3=async()=>{
+    const Dadisontheway=new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve('person3:Got ticket')
+        },2000)
     })
-})
-const addButter=getPopcorn.then((t)=>{
-    return new Promise((resolve,reject)=>{
-        resolve(`${t}, added butter`)
-    })
-})
-const getColdDrinks =addButter.then((t)=>{
-    return new Promise((resolve,reject)=>{
-        resolve(`${t}, got cold drinks and Entered theatre`)
-    })
-})
-getColdDrinks.then((t)=>{
+    let ticket=await Dadisontheway;
+    const getPopcorn= new Promise((resolve,reject)=>resolve(`${ticket}, bought Popcorn`))
+    let Popcorn=await getPopcorn;
+    //console.log(Popcorn)
+    const addButter=new Promise((resolve,reject)=>resolve(`${Popcorn}, added butter`))
+    let butter=await addButter;  
+    //console.log(butter)
+    const getColdDrinks =new Promise((resolve,reject)=>resolve(`${butter}, got cold drinks and Entered theatre`))
+    let colddrinks=await getColdDrinks;
+    //console.log(colddrinks)
+    return colddrinks   
+}    
+    
+person3().then((t)=>{
     console.log(t)
 })
 console.log("person4:Entered theatre")
